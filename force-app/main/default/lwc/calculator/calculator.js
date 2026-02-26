826219788d279496c54c8cc69f7024267bef54ff
Atharva Billore <billoreatharva22@gmail.com>
2026-02-26 16:16:30 +0530

    Calculator

---
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -0,0 +1,28 @@
+import { LightningElement } from 'lwc';
+export default class Calculator extends LightningElement {
+    numberone = "";
+    numbertwo = "";
+    result = 0;
+    changeHandlerNumber (event) {
+        let name = event.target.name;
+        let value = event.target.value;
+        if(name === "number1"){
+            this.numberone = value;
+        }else if(name === 'number2'){
+            this.numbertwo = value;
+        }
+    }
+    addHandler(event) {
+        this.result =  parseInt(this.numberone) + parseInt(this.numbertwo);
+    }
+    subHandler(event) {
+        this.result =  parseInt(this.numberone) - parseInt(this.numbertwo);
+    }
+    mulHandler(event) {
+        this.result =  parseInt(this.numberone) * parseInt(this.numbertwo);
+    }
+    divHandler(event) {
+        this.result =  parseInt(this.numberone) / parseInt(this.numbertwo);
+    }
+    
+}
